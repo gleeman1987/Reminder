7719214733b8215111cab852c4ed85ca00649df9
郑军 <zhengjun1987@outlook.com>
2016-12-19 23:48:50 +0800

    Author:Zheng Jun Date:2016-12-19 23:48 Description:modification of Reminders project

--- app/src/main/java/com/example/zhengjun/reminder/RemindersActivity.java
+++ app/src/main/java/com/example/zhengjun/reminder/RemindersActivity.java
@@ -1,14 +1,23 @@
 package com.example.zhengjun.reminder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RemindersActivity extends AppCompatActivity {
+
+    @Bind(R.id.lv_reminders)
+    ListView lvReminders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
-
+        ButterKnife.bind(this);
+        lvReminders.setAdapter(new ArrayAdapter<String>(this,R.layout.reminders_row,R.id.row_text,new String[]{"first record","second record","third record"}));
     }
 }
